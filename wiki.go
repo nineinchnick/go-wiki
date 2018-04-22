3be7d12ffc9bc0a81616c1532435ac4b2377070e
John Was <janek.jan@gmail.com>
2018-04-22 13:28:40 +0200

    initial import

---
+++ wiki.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"errors"
+	"html"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func filename(title string) string {
+	return html.EscapeString(title) + ".md"
+}
+
+func (p *Page) save() error {
+	filename := filename(p.Title)
+	err := ioutil.WriteFile(filename, p.Body, 0600)
+	if err != nil {
+		log.Printf("ERROR Saving %s", filename)
+	}
+	return err
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := filename(title)
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Printf("INFO Missing file %s", filename)
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		log.Printf("ERROR Executing template %s on %s", tmpl, p.Title)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)/?$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		log.Printf("ERROR Getting title from URL %s", r.URL.Path)
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	_, title := m[1], m[2]
+	return title, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		log.Printf("INFO Creating %s", title)
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err = p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
